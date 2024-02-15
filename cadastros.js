e897b8c57e6672f000fb7d01a01ed28dbd2e6b9f
Seu Nome <mosico1442@fkcod.com>
2024-02-15 11:34:26 -0300

    Adequação para cadastrar em múltiplas listas de presente

--- cadastros.js
+++ cadastros.js
@@ -1,96 +1,25 @@
 //Mostrar o cadastro
 var presentesContainer = document.querySelector(".body-cadastro");
   setTimeout(() => {presentesContainer.classList.add("show-container"), 50});
+
 /*Botão Concluir*/
 document.getElementById('submit-button').addEventListener('click', verificaCampoObrigatorio)
-
-function verificaCampoObrigatorio(evento) {
-
-    //Percorrer os campos de input
-    for(let i = 0; i < input.length; i++){
-
-     //Verificar se o campo é obrigatório
-        var campo = document.querySelectorAll('input')[i];
-        var obrigatorio = campo.required;
-        var campoValor = campo.value;
-        var campoStyle = campo.style
-        var idDiv = campo.parentNode.id;
-        var contadorCamposEmBranco = 0
-
-        if(obrigatorio == true){
-            if(campoValor == ''){
-                contadorCamposEmBranco = contadorCamposEmBranco + 1;
-            }
-            
-            //se sim, verificar se o valor do campo foi preenchido. Se não foi preenchido indica o campo como obrigatório
-            if(campoValor == '' && campoStyle.border != '1px solid red'){
-                
-                campo.style="border: 1px solid #ccc";
-                campo.style="border: 1px solid red";
-                var mensagemVermelha = document.createElement('p');
-                    mensagemVermelha.classList.add('mensagemVermelha')
-                    mensagemVermelha.id = `id${i}`
-                    mensagemVermelha.setAttribute('for', input[i].id)
-                    mensagemVermelha.textContent = 'Obrigatório*'
-
-                document.getElementById(idDiv).appendChild(mensagemVermelha);
-           } 
-           
-        }
-            
-    }    
-    
-    if(contadorCamposEmBranco == 0){
-        cadastrarProduto()
-    }
-};        
-    
-function tirarMensagemVermelha(idCampo){
-   
-        let mensagem = document.querySelector(`p[for=${idCampo}]`) 
-        let inputTirar = document.querySelector(`#${idCampo}`)   
-        var cor = "1px solid red";
-        var campoBorda = inputTirar.style.border;
-        
-        if(mensagem){
-            mensagem.remove()
-        }
-
-        if(campoBorda == cor){
-            inputTirar.style="border: 1px solid #ccc";
-        }
-       
-};        
-
-function teclaPressionada(e, idCampo) {
-
-    
-    var tecla = e.key;
-    var tabIndex = document.querySelector(`#${idCampo}`).tabIndex
-    var proximoCampo = document.querySelector(`[tabIndex="${tabIndex + 1}"]`)
-    
-    if(tecla == 'Enter'){
-        e.preventDefault()
-        proximoCampo.focus()
-    }
-
-}
-
+ 
 function cadastrarProduto(){
     loading('exibir');
-    var nomeProduto = input[0].value;
-    var link = input[1].value;
-    var img = input[2].value;
-    var situacao = 0;
-    var apiUrl = 'https://script.google.com/macros/s/AKfycbwAalhIoCgV2HRVLf1VeKvYCzihXhGGS4fi3CMi_WyUXZQecIvIfG31sqt5eJRzcEOz/exec?path=cadastro&' + new Date().getTime();
-    var body = {
+    const nomeProduto = input[0].value;
+    const link = input[1].value;
+    const img = input[2].value;
+    const qtd = input[3].value;
+    const local = document.querySelector('#selectLista').options.selectedIndex;
+    const apiUrl = `https://script.google.com/macros/s/AKfycbwAalhIoCgV2HRVLf1VeKvYCzihXhGGS4fi3CMi_WyUXZQecIvIfG31sqt5eJRzcEOz/exec?path=cadastro&local=${local}&${new Date().getTime()}`;
+    const body = {
         "Descricao":nomeProduto,
         "Link":link,
         "Linkimg":img,
-        "Situacao":0
+        "QtdMax":parseInt(qtd, 10),
+        "Situacao":'0'
     };
-
-    var dadosRecebidos = null;
 
     const configuracao = {
         method: 'POST', // Método HTTP
@@ -145,7 +74,8 @@ function loading(acao){
     function limparInputs(){
         //Limpando os campos se o Status for positivo
         for(i=0; i < input.length; i++){
-            input[i].value = ''
+            input[i].value = '';
+            document.querySelector('#selectLista').options.selectedIndex = 0;  
         }
     };
     
--- main.js
+++ main.js
@@ -1,26 +1,82 @@
 var input = document.querySelectorAll('input');
+var listaCamposSelecionaveis = document.querySelectorAll('[tabindex]')
 
-/*Digitar em qualquer input*/
-for(let i = 0; i < input.length; i++){
-let idCampo = input[i].id
-input[i].addEventListener('input', () => {tirarMensagemVermelha(idCampo)})
-}
 
 /*Clique em teclas*/
-for(let i = 0; i < input.length; i++){
-    let idCampo = input[i].id;
+for(let i = 0; i < listaCamposSelecionaveis.length; i++){
+    let idCampo = listaCamposSelecionaveis[i].id;
     let idProximoCampo = 0
-    if(i + 1 >= input.length){
-        idProximoCampo = input[input.length - 1].id
+    if(i + 1 >= listaCamposSelecionaveis.length){
+        idProximoCampo = listaCamposSelecionaveis[listaCamposSelecionaveis.length - 1].id
     }
     else{
-        idProximoCampo = input[i + 1].id
+        idProximoCampo = listaCamposSelecionaveis[i + 1].id
     }
     
-    input[i].addEventListener('keydown', (e) => {teclaPressionada(e, idCampo)})
+    listaCamposSelecionaveis[i].addEventListener('keydown', (e) => {teclaPressionada(e, idCampo)})
     }
 
-    /*Ocultar Cadastro ao sair da página*/
+    function teclaPressionada(e, idCampo) {
+        var tecla = e.key;
+        var tabIndex = document.querySelector(`#${idCampo}`).tabIndex
+        var proximoCampo = document.querySelector(`[tabIndex="${tabIndex + 1}"]`)
+        
+        if(tecla == 'Enter'){
+          const campoSelecionado = document.getElementById(idCampo);
+            if(campoSelecionado.nodeName == 'INPUT'){
+                e.preventDefault();
+                proximoCampo.focus()
+            }    
+        }
+    
+    }
+ 
+    var contadorCamposEmBranco = 0
+
+    function verificaCampoObrigatorio() {
+        const camposObrigatorios = document.querySelectorAll('[required]');
+
+        camposObrigatorios.forEach(campo => {
+            const campoValor = campo.value;
+            const selectValor = document.querySelector('#selectLista').options.selectedIndex;
+            const campoTipo = campo.nodeName;
+            const mensagemExiste = document.querySelectorAll(`.mensagemVermelha[for="${campo.id}"]`);
+    
+            if (campoTipo === 'INPUT' && campoValor === '' && mensagemExiste.length === 0) {
+                campo.style.borderColor = 'red';
+                campo.parentElement.insertAdjacentHTML('beforeend', `<p class="mensagemVermelha" for="${campo.id}">Obrigatório*</p>`);
+                campo.addEventListener('input', tirarMensagemVermelha);
+                contadorCamposEmBranco ++
+            }
+            // ...
+            else if (campoTipo === 'SELECT' && selectValor === 0 && mensagemExiste.length === 0) {
+                campo.style.borderColor = 'red';
+                campo.parentElement.insertAdjacentHTML('beforeend', `<p class="mensagemVermelha" for="${campo.id}">Obrigatório*</p>`);
+                campo.addEventListener('input', tirarMensagemVermelha);
+                contadorCamposEmBranco ++
+            }
+        });
+
+        if(contadorCamposEmBranco == 0){
+            cadastrarProduto();
+        }
+    };
+    
+    // Função para remover mensagem vermelha e event listener
+    function tirarMensagemVermelha(event) {
+        let mensagem = document.querySelector(`.mensagemVermelha[for="${event.target.id}"`) ;
+        let inputTirar = document.querySelector(`#${event.target.id}`);
+    
+        if (mensagem) {
+            mensagem.remove();
+            inputTirar.removeAttribute('style');
+            inputTirar.removeEventListener('input', tirarMensagemVermelha);
+            contadorCamposEmBranco --
+        }
+    };
+    
+
+    /*Ocultar container Cadastro ao sair da página, para quando voltar, fazer a animação*/
 
     const buttonPresentes = document.querySelectorAll('nav-link');
         buttonPresentes.forEach(item => {item.addEventListener('click', () => {document.querySelector('#cadastro-container').classList.remove('show-container')})});
